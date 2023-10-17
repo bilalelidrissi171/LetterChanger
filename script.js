fe8fbf65471aa8b5c4395aa817b6b556518af238
Bilal El Idrissi <bel-idri@e3r3p13.1337.ma>
2023-10-17 23:19:09 +0100

    init

--- script.js
+++ script.js
@@ -2,18 +2,15 @@
 function arabic() {
 	var t1 = document.getElementById("t1").value;
 	for (var i = 0; i < t1.length; i++) {
-		t1 = t1.replace("ا", "|");
 		t1 = t1.replace("أ", "|");
 		t1 = t1.replace("ج", "g");
 		t1 = t1.replace("ح", "7");
 		t1 = t1.replace("ز", "z");
-		t1 = t1.replace("س", "s");
 		t1 = t1.replace("ط", "T");
 		t1 = t1.replace("ق", "9");
 		t1 = t1.replace("ه", "8");
 		t1 = t1.replace("إ", "|");
 		t1 = t1.replace("آ", "I");
-
 	}
 	document.getElementById("t2").value = t1;
 }
